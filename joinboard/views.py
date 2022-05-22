6e0cb87d8f8825511d8ba2ae22044a96bef50445
Sebastian Ebel <sebastianebel@hotmail.de>
2022-05-22 17:37:11 +0200

    feedback task post request added

--- joinboard/views.py
+++ joinboard/views.py
@@ -23,6 +23,9 @@ def allTask(request):
     """
     this function returns the completely JSON
     """
+    if request.method == 'POST':
+        Task.objects.create(text=request.POST['textvalue'], user=request.user, created_at=request.POST['datevalue'], description=request.POST['description'], category=request.POST['category'])
+        return HttpResponse('Task is create')
     if request.method == 'GET':
         alljson = Task.objects.all()
         serialized_obj = serializers.serialize('json',  alljson, )
@@ -78,5 +81,8 @@ def logout_view(request):
     return HttpResponseRedirect(redirect)
 
 def back_view(request):
+    """
+    This function is for the return to the login.
+    """
     redirect = request.POST.get('next', '/')
     return HttpResponseRedirect(redirect)
